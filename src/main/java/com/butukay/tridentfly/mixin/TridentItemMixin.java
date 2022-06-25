b8d825b81231feaf29cd850a8c2fbb4f7d45225d
Butukay <vlad.imangulov@icloud.com>
2022-06-25 21:01:00 +0300

    Migrated to 1.18.2

--- src/main/java/com/butukay/tridentfly/mixin/TridentItemMixin.java
+++ src/main/java/com/butukay/tridentfly/mixin/TridentItemMixin.java
@@ -50,58 +50,62 @@ public abstract class TridentItemMixin extends Item implements Vanishable {
 
     @Overwrite
     public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-        PlayerEntity playerEntity = (PlayerEntity) user;
+        if (user instanceof PlayerEntity) {
+            PlayerEntity playerEntity = (PlayerEntity) user;
+            int i = this.getMaxUseTime(stack) - remainingUseTicks;
+            if (i >= 2) {
+                int j = EnchantmentHelper.getRiptide(stack);
+                if (!world.isClient) {
+                    stack.damage(1, playerEntity, (p) -> {
+                        p.sendToolBreakStatus(user.getActiveHand());
+                    });
+                    if (j == 0) {
+                        TridentEntity tridentEntity = new TridentEntity(world, playerEntity, stack);
+                        tridentEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, 2.5F + (float) j * 0.5F, 1.0F);
+                        if (playerEntity.getAbilities().creativeMode) {
+                            tridentEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                        }
 
-        int j = EnchantmentHelper.getRiptide(stack);
-
-        if (!world.isClient) {
-            stack.damage(1, (LivingEntity) playerEntity, (Consumer<LivingEntity>) ((p) -> {
-                p.sendToolBreakStatus(user.getActiveHand());
-            }));
-            if (j == 0) {
-                TridentEntity tridentEntity = new TridentEntity(world, playerEntity, stack);
-                tridentEntity.setProperties(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, 2.5F + (float) j * 0.5F, 1.0F);
-                if (playerEntity.getAbilities().creativeMode) {
-                    tridentEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                        world.spawnEntity(tridentEntity);
+                        world.playSoundFromEntity((PlayerEntity) null, tridentEntity, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                        if (!playerEntity.getAbilities().creativeMode) {
+                            playerEntity.getInventory().removeOne(stack);
+                        }
+                    }
                 }
 
-                world.spawnEntity(tridentEntity);
-                world.playSoundFromEntity((PlayerEntity) null, tridentEntity, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                if (!playerEntity.getAbilities().creativeMode) {
-                    playerEntity.getInventory().removeOne(stack);
+                playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                float f = playerEntity.getYaw();
+                float g = playerEntity.getPitch();
+                float h = -MathHelper.sin(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
+                float k = -MathHelper.sin(g * 0.017453292F);
+                float l = MathHelper.cos(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
+                float m = MathHelper.sqrt(h * h + k * k + l * l);
+                float n = 3.0F * ((1.0F + (float) j) / 4.0F);
+                h *= n / m;
+                k *= n / m;
+                l *= n / m;
+                playerEntity.addVelocity((double) h, (double) k, (double) l);
+                playerEntity.useRiptide(20);
+                if (playerEntity.isOnGround()) {
+                    float o = 1.1999999F;
+                    playerEntity.move(MovementType.SELF, new Vec3d(0.0D, 1.1999999284744263D, 0.0D));
                 }
-            }
-        }
 
-        playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
-        if (j > 0) {
-            float f = playerEntity.getYaw();
-            float g = playerEntity.getPitch();
-            float h = -MathHelper.sin(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
-            float k = -MathHelper.sin(g * 0.017453292F);
-            float l = MathHelper.cos(f * 0.017453292F) * MathHelper.cos(g * 0.017453292F);
-            float m = MathHelper.sqrt(h * h + k * k + l * l);
-            float n = 3.0F * ((1.0F + (float) j) / 4.0F);
-            h *= n / m;
-            k *= n / m;
-            l *= n / m;
-            playerEntity.addVelocity((double) h, (double) k, (double) l);
-            playerEntity.setRiptideTicks(20);
-            if (playerEntity.isOnGround()) {
-                float o = 1.1999999F;
-                playerEntity.move(MovementType.SELF, new Vec3d(0.0D, 1.1999999284744263D, 0.0D));
-            }
+                SoundEvent soundEvent;
+                if (j >= 3) {
+                    soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_3;
+                } else if (j == 2) {
+                    soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_2;
+                } else {
+                    soundEvent = SoundEvents.ITEM_TRIDENT_RIPTIDE_1;
+                }
 
-            SoundEvent soundEvent3;
-            if (j >= 3) {
-                soundEvent3 = SoundEvents.ITEM_TRIDENT_RIPTIDE_3;
-            } else if (j == 2) {
-                soundEvent3 = SoundEvents.ITEM_TRIDENT_RIPTIDE_2;
-            } else {
-                soundEvent3 = SoundEvents.ITEM_TRIDENT_RIPTIDE_1;
-            }
+                world.playSoundFromEntity((PlayerEntity) null, playerEntity, soundEvent, SoundCategory.PLAYERS, 1.0F, 1.0F);
 
-            world.playSoundFromEntity((PlayerEntity) null, playerEntity, soundEvent3, SoundCategory.PLAYERS, 1.0F, 1.0F);
+
+            }
         }
     }
 }
+
